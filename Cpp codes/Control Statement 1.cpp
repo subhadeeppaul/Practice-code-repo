f480610760dcf5ee60dff84d51957f1d96422e90
Vikram S <vikram.senthil@outlook.com>
2020-10-01 10:08:30 +0530

    Control Statement

---
+++ Cpp codes/Control Statement 1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main() {
+int n;
+  cin>>n;
+  for(int i=1;i<=n;i++)
+  {
+    if(i==6 or i==9)
+    {
+      continue;
+    }
+    else
+    {
+      cout<<i;
+   
+      
+    }
+    cout<<" ";
+  }
+  return 0;
+}
