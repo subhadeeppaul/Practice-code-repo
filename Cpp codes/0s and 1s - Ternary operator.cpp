cba985a712b38c09d5a3fb2c9a2602cf5640c9d2
Vikram S <vikram.senthil@outlook.com>
2020-10-01 10:03:26 +0530

    0s and 1s

---
+++ Cpp codes/0s and 1s - Ternary operator.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cin>>n;
+	int val=0;
+	for(int i=1;i<=n;i++)
+	{
+		for(int j=1;j<=i;j++)
+		{
+			cout<<val;
+		val?val=0:val=1;
+		}
+		cout<<endl;
+		
+	}
+	return 0;
+}
